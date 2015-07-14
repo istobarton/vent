d4393852ae8992b0bca29a55cfe20955783d1c6e
Isto <istobarton@gmail.com>
2015-07-14 16:55:08 -0500

    Added vent count

--- client/type.js
+++ client/type.js
@@ -5,15 +5,17 @@ angular.module('vent.type', [])
 
   $scope.getData = function(type, $location){
     console.log($scope.type.rant);
-    $scope.type.rant = "";
-    // .then(function(){
-    //   $location.path('/')
-    // })
-    // .catch(function(error){
-    //   console.log(error)
-    // });
+
+  $scope.type.rant = "";
+  };
+
+  $scope.num = function(type){
+    var hold = $scope.type.rant.split(" ").length-1 || 0;
+    var sum = 750 - hold;
+    $scope.count = " " + sum + " words till calm"
+  };
   }
-})
+  )
 
 .controller('AuthController', function ($scope, $window, $location) {
   $scope.user = {};
